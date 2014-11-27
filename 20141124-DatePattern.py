04bd2bd852411d0833ce811bd86675db67526827
MichalKutyla <michal.kutyla.87@gmail.com>
2014-11-27 20:59:04 +0100

    First two Katas in Python

---
+++ 20141124-DatePattern.py
@@ -0,0 +1,77 @@
+__author__ = 'SG0220823'
+
+import unittest
+import datetime
+
+
+class DataPatternTests(unittest.TestCase):
+    def testMatches(self):
+        p = DatePattern(2004, 9, 28)
+        d = datetime.date(2004, 9, 28)
+        self.failUnless(p.matches(d))
+
+    def testDoesntMatch(self):
+        p = DatePattern(2004, 9, 28)
+        d = datetime.date(2003, 9, 28)
+        self.failIf(p.matches(d))
+
+    def testMatchesYearAsWildCard(self):
+        p = DatePattern(0, 9, 28)
+        d = datetime.date(2003, 9, 28)
+        self.failUnless(p.matches(d))
+
+    def testMatchesYearAndMonthAsWildCards(self):
+        p = DatePattern(0, 0, 28)
+        d = datetime.date(2003, 9, 28)
+        self.failUnless(p.matches(d))
+
+    def testMatchesYearAndMonthAndDayAsWindCards(self):
+        p = DatePattern(0, 0, 0)
+        d = datetime.date(2003, 9, 28)
+        self.failUnless(p.matches(d))
+
+    def testMatchesWeekday(self):
+        p = DatePattern(0, 0, 0, 2)
+        d = datetime.date(2004, 9, 29)
+        self.failUnless(p.matches(d))
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
+
+
+class DatePattern(object):
+    def __init__(self, year, month, day, weekday=0):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.weekday = weekday
+
+
+    def yearMatches(self, date):
+        return self.fieldMatches(self.year, date.year)
+
+    def monthMatches(self, date):
+        return self.fieldMatches(self.month, date.month)
+
+    def dayMatches(self, date):
+        return self.fieldMatches(self.day, date.day)
+
+    def weekdayMatches(self, date):
+        return self.fieldMatches(self.weekday, date.weekday())
+
+    def fieldMatches(self, patternField, inputField):
+        return patternField == inputField or not patternField
+
+    def matches(self, date):
+        return (self.yearMatches(date) and
+                self.monthMatches(date) and
+                self.dayMatches(date) and
+                self.weekdayMatches(date))
+
+
+
---
+++ 20141127-SelectionSort.py
@@ -0,0 +1,39 @@
+__author__ = 'SG0220823'
+
+import unittest
+
+
+def sort(a):
+    for i in range(len(a)):
+        min_ind = i
+        for j in range(i + 1, len(a)):
+            if a[min_ind] > a[j]:
+                min_ind = j
+        swap(a, i, min_ind)
+    return a
+
+
+def swap(a, i, j):
+    tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+
+
+class SelectionSortTests(unittest.TestCase):
+    def test_sort_empty(self):
+        a = []
+        self.failUnlessEqual(sort(a), [])
+
+    def test_sort_one(self):
+        a = [1]
+        self.failUnlessEqual(sort(a), [1])
+
+    def test_sort_two(self):
+        a = [2, 1]
+        self.failUnlessEqual(sort(a), [1, 2])
+
+    def test_sort_three(self):
+        a = [2, 1, 3]
+        self.failUnlessEqual(sort(a), [1, 2, 3])
+
+
